34b81d9e1f0a2ad3dbb8f4f3132f6f78dcfc24a7
gautamKumar <gautamkannoujiya58@gmail.com>
2024-08-23 02:43:21 +0530

    updated for failing test cases for logo and other

--- qtify/src/components/Logo/Logo.js
+++ qtify/src/components/Logo/Logo.js
@@ -1,6 +1,9 @@
 import React from "react";
 import LogoImage from "../../assets/logoImage.png";
+import styles from "../Logo/Logo.module.css";
 
 export default function Logo() {
-  return <img src={LogoImage} alt="logo" width={67} />;
+  return (
+    <img className={styles.imgTag} src={LogoImage} alt="logo" width={67} />
+  );
 }
